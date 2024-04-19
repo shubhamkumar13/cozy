146cb6234da8478eea661f93c62e1f895d820fc3
Shubham Kumar <kumar.shubham.off@gmail.com>
2024-04-19 18:03:26 +0530

    update src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env::current_dir, io::Write, path::PathBuf};
+use std::{env::current_dir, io::Write, path::PathBuf, str::FromStr};
 
 use anyhow::Result;
 use clap::{value_parser, Arg, ArgMatches, Command, Subcommand};
@@ -8,13 +8,31 @@ fn main() -> Result<()> {
     let path_str = current_dir()?.as_os_str().to_string_lossy().to_string();
 
     let cmd = Command::new("cozy").subcommand(init(path_str));
-
-    let binding = cmd.get_matches();
-    init_matches(binding)?;
+    init_matches(cmd)?;
     Ok(())
 }
 
-struct File;
+struct File {
+    name: String,
+    dir_path: PathBuf,
+    file: std::fs::File,
+}
+
+impl File {
+    fn new(name: String, dir_path: PathBuf, contents: String) -> Result<Self> {
+        let mut file = Self::touch(&dir_path.join(name.clone()))?;
+        file.write(contents.as_bytes())?;
+        Ok(Self {
+            name,
+            dir_path,
+            file,
+        })
+    }
+    fn touch(path: &PathBuf) -> Result<std::fs::File> {
+        std::fs::File::create(path).map_err(Into::into)
+    }
+}
+
 struct Dir;
 
 struct Project {
@@ -24,52 +42,99 @@ struct Project {
 }
 
 impl Project {
-    fn new(dune_project: String, opam_file: String, bin: String) {
-        todo!()
-    }
-    fn create_dune_project() {
-        let dune_project_contents = dune_project(name.clone())?;
-        let dune_project_file = dir_path.join("dune_project");
-        let mut dune_project_file = touch(&dune_project_file)?;
-        dune_project_file.write(dune_project_contents.as_bytes())?;
-        todo!()
-    }
-    fn create_opam_file() {
-        let mut opam_file = String::new();
-        opam_file.push_str(name.clone().as_str());
-        opam_file.push_str(".opam");
+    fn new(name: String, dir_path: PathBuf) -> Result<Self> {
+        fn create_dune_project(name: &String, dir_path: &PathBuf) -> Result<File> {
+            fn dune_project(name: String) -> Result<String> {
+                let output: String = formatdoc! {r#"
+                (lang dune 3.14)
 
-        let opam_file = touch(&dir_path.join(opam_file))?;
-        todo!()
-    }
-    fn create_bin_dir() {
-        let bin_dir = mkdir(&dir_path.join("bin"))?;
-        // main.ml file inside bin
-        let main_ml_contents = main_ml()?;
-        let mut main_ml_file = touch(&dir_path.join("bin").join("main.ml"))?;
-        main_ml_file.write(main_ml_contents.as_bytes())?;
-        todo!()
+                (name {name})
+
+                (generate_opam_files true)
+
+                (source
+                (github username/reponame))
+
+                (authors "Author Name")
+
+                (maintainers "Maintainer Name")
+
+                (license LICENSE)
+
+                (documentation https://url/to/documentation)
+
+                (package
+                (name {name})
+                (synopsis "A short synopsis")
+                (description "A longer description")
+                (depends ocaml dune)
+                (tags
+                (topics "to describe" your project)))
+
+                ; See the complete stanza docs at https://dune.readthedocs.io/en/stable/dune-files.html#dune-project
+            "#};
+
+                Ok(output)
+            }
+            let dune_project_contents = dune_project(name.clone())?;
+            File::new(name.clone(), dir_path.clone(), dune_project_contents)
+        }
+        fn create_opam_file(name: &String, dir_path: &PathBuf) -> Result<File> {
+            let mut opam_file = String::new();
+            opam_file.push_str(name.clone().as_str());
+            opam_file.push_str(".opam");
+            File::new(opam_file, dir_path.clone(), String::new())
+        }
+        fn create_bin_dir(dir_path: &PathBuf) -> Result<File> {
+            let bin_dir = dir_path.join("bin");
+            mkdir(&bin_dir)?;
+            // main.ml file inside bin
+            fn main_ml() -> Result<&'static str> {
+                Ok(r#"let () = print_endline "Hello, World!""#)
+            }
+            let main_ml_contents = main_ml()?.into();
+            File::new("main.ml".into(), bin_dir, main_ml_contents)
+        }
+        create_bin_dir(&dir_path)?;
+        Ok(Self {
+            dune_project: create_dune_project(&"dune-project".to_string(), &dir_path)?,
+            opam_file: create_opam_file(&name, &dir_path)?,
+            bin: None,
+        })
     }
 }
 
-fn init_matches(binding: ArgMatches) -> Result<()> {
+fn init_matches(cmd: Command) -> Result<()> {
+    let mut cmd_ = cmd.clone();
+    let binding = cmd.get_matches();
+
     let name: String = match binding.subcommand() {
         Some(("init", matches)) => matches
             .get_one::<String>("name")
             .into_iter()
             .map(|x| x.clone())
             .collect(),
+        None => {
+            cmd_.print_long_help()?;
+            "".to_string()
+        }
         _ => unreachable!("Somehow the error for name arg didn't work so Idk what happened"),
     };
-    let path: PathBuf = match binding.subcommand() {
-        Some(("init", matches)) => matches.get_one::<PathBuf>("path").into_iter().collect(),
+    match binding.subcommand() {
+        Some(("init", matches)) => {
+            let path: PathBuf = matches.get_one::<PathBuf>("path").into_iter().collect();
+
+            // create a file in current_dir + name
+            let dir_path = path.clone().join(PathBuf::from(name.clone()));
+            if !dir_path.exists() {
+                mkdir(&dir_path)?;
+            }
+
+            Project::new(name, dir_path)?;
+        }
+        None => (),
         _ => unreachable!("This is not a subcommand or you shouldn't be here"),
     };
-    // create a file in current_dir + name
-    let dir_path = path.clone().join(PathBuf::from(name.clone()));
-    if !dir_path.exists() {
-        mkdir(&dir_path)?;
-    }
 
     Ok(())
 }
@@ -87,49 +152,6 @@ fn init(path_str: String) -> impl Into<Command> {
         .arg(Arg::new("name").required(true))
 }
 
-fn touch(path: &PathBuf) -> Result<std::fs::File> {
-    std::fs::File::create(path).map_err(Into::into)
-}
-
 fn mkdir(dir: &PathBuf) -> Result<()> {
     std::fs::create_dir(dir).map_err(Into::into)
-}
-
-fn dune_project(name: String) -> Result<String> {
-    let output: String = formatdoc! {r#"
-        (lang dune 3.14)
-
-        (name {name})
-
-        (generate_opam_files true)
-
-        (source
-        (github username/reponame))
-
-        (authors "Author Name")
-
-        (maintainers "Maintainer Name")
-
-        (license LICENSE)
-
-        (documentation https://url/to/documentation)
-
-        (package
-        (name {name})
-        (synopsis "A short synopsis")
-        (description "A longer description")
-        (depends ocaml dune)
-        (tags
-        (topics "to describe" your project)))
-
-        ; See the complete stanza docs at https://dune.readthedocs.io/en/stable/dune-files.html#dune-project
-    "#};
-
-    Ok(output)
-}
-
-fn main_ml() -> Result<&'static str> {
-    Ok(r#"
-    let () = print_endline "Hello, World!"
-    "#)
 }
